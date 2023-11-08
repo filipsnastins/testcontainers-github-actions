ce54ec2a8a81b5959df13d95f107303c9171eb5d
Filips Nastins <nastinsfilips@gmail.com>
2023-11-08 20:52:33 +0100

    test: start testcontainer once per test session

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,43 @@
-from typing import AsyncGenerator, Generator, cast
+import asyncio
+import contextlib
+from typing import AsyncGenerator, Generator, Iterator, cast
 
 import httpx
 import pytest
 import pytest_asyncio
-from docker.models.images import Image as DockerImage
+from docker.models.images import Image
 from tomodachi_testcontainers import MotoContainer, TomodachiContainer
 from tomodachi_testcontainers.clients import SNSSQSTestClient
 from tomodachi_testcontainers.utils import get_available_port
+from types_aiobotocore_sns import SNSClient
+from types_aiobotocore_sqs import SQSClient
 
 
-@pytest_asyncio.fixture()
-async def _create_topics_and_queues(moto_snssqs_tc: SNSSQSTestClient) -> None:
-    await moto_snssqs_tc.subscribe_to(
+@pytest.fixture(scope="session")
+def event_loop() -> Iterator[asyncio.AbstractEventLoop]:
+    with contextlib.closing(asyncio.new_event_loop()) as loop:
+        yield loop
+
+
+@pytest.fixture(scope="session")
+def snssqs_tc(moto_sns_client: SNSClient, moto_sqs_client: SQSClient) -> SNSSQSTestClient:
+    return SNSSQSTestClient.create(moto_sns_client, moto_sqs_client)
+
+
+@pytest_asyncio.fixture(scope="session")
+async def _create_topics_and_queues(snssqs_tc: SNSSQSTestClient) -> None:
+    await snssqs_tc.subscribe_to(
         topic="order--created",
         queue="order--created",
     )
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def tomodachi_container(
-    tomodachi_image: DockerImage,
-    moto_container: MotoContainer,
-    _create_topics_and_queues: None,
-    _reset_moto_container_on_teardown: None,
+    testcontainers_docker_image: Image, moto_container: MotoContainer, _create_topics_and_queues: None
 ) -> Generator[TomodachiContainer, None, None]:
     with (
-        TomodachiContainer(image=str(tomodachi_image.id), edge_port=get_available_port())
+        TomodachiContainer(image=str(testcontainers_docker_image.id), edge_port=get_available_port())
         .with_env("AWS_REGION", "us-east-1")
         .with_env("AWS_ACCESS_KEY_ID", "testing")
         .with_env("AWS_SECRET_ACCESS_KEY", "testing")
@@ -37,7 +49,7 @@ def tomodachi_container(
         yield cast(TomodachiContainer, container)
 
 
-@pytest_asyncio.fixture()
+@pytest_asyncio.fixture(scope="session")
 async def http_client(tomodachi_container: TomodachiContainer) -> AsyncGenerator[httpx.AsyncClient, None]:
     async with httpx.AsyncClient(base_url=tomodachi_container.get_external_url()) as client:
         yield client
